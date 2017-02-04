c1302b614bdb670720b7ddf5a12c0d4fd7d84524
Abdul Malik Ikhsan <samsonasik@gmail.com>
2017-02-04 16:51:31 +0700

    make sure no output during function call

--- src/IsDeprecated.php
+++ src/IsDeprecated.php
@@ -7,6 +7,7 @@ use Zend\Stdlib\ErrorHandler;
 
 function isDeprecated(string $function, array $parameters = [], $object = null): bool
 {
+    ob_start();
     ErrorHandler::start(E_USER_DEPRECATED);
     if (is_object($object)) {
         $object->$function(...$parameters);
@@ -15,6 +16,7 @@ function isDeprecated(string $function, array $parameters = [], $object = null):
         $function(...$parameters);
     }
     $result = ErrorHandler::stop();
+    ob_clean();
 
     if ($result instanceof ErrorException) {
         return true;
